7b98a476695166820eaa69540d01d71603d33a28
jesusssGM <124410589+jesusssGM@users.noreply.github.com>
2023-09-06 19:18:24 -0700

    Add files via upload

--- parcial1/pares.js
+++ parcial1/pares.js
@@ -1,8 +1,27 @@
 $(document).ready(function ()
-{
-    var contador = 0;
+{   
+    var carta1 = ""; var carta2 = "";
+    var par = false;
+    var total_pares = 0; 
+
     $("img").click(function (e){
-        var nombre_imagen =$(this).attr('data-id')
-        $(this).attr('src','imagenes/' + nombre_imagen);
+        
+        var estado = $(this).attr('data-estado')
+        var nombre_imagen = $(this).attr('data-id')
+        if(estado=="0"){
+            if(carta1 == ""){
+                console.log("estado de la carta:" + estado)
+                carta1 = $(this);
+                carta1.attr('src', 'imagenes/' + nombre_imagen)
+                console.log("se ignoro la carta 1: ")
+               
+            }else{
+                carta2 =$(this);
+                carta1.attr('src', 'imagenes/' + nombre_imagen)
+                console.log("se ignoro la carta 2: ")
+            }
+        }
+
+       
     });
 });
